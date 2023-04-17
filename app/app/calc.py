a9bbee094684e3f17289a2a96202350a82f339a4
AmosBunde <git.bunde@hotmail.com>
2023-04-17 21:30:33 +0300

    simple introduction to TDD

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions   
+"""
+def add_numbers(a, b):
+    """Add a and b and return results"""
+    return a + b
+
+
+def subtract(f,g):
+    """Subtract value f and g"""
+    return f - g
